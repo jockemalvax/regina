993ed3eadb2e9a65134aeea405db295d2a97373f
Joakim Erlandsson <jockemalvax@gmail.com>
2016-09-17 22:35:15 +0200

    Backup

--- index.php
+++ index.php
@@ -29,6 +29,164 @@ a:active {
 .container #about div p {
 	font-family: Gotham, Helvetica Neue, Helvetica, Arial, sans-serif;
 }
+
+
+#div-1 {
+ position:relative;
+ top:25px;
+    width: 100%;
+    height: 430px;
+	background-color:#8bc34a;
+}
+#div-1a {
+ position:absolute;
+ top:0px;
+ right:0px;
+ width:50%;
+ height: 400px;
+ padding-top: 15px;
+    padding-right: 0px;
+    padding-bottom: 25px;
+    padding-left: 80px;
+ }
+#div-1b {
+ position:absolute;
+ top:5px;
+ left:0;
+ width:50%;
+}
+#text_row1 {
+ font-family: Arial,Gotham, Helvetica Neue, Helvetica,sans-serif;
+ font-weight:bold;
+ font-size:45px;	
+ color:#ffffff;
+ position:relative;
+ top:0px;
+ left:10px;
+ width:100%;
+ height: 25px;
+ padding-top: 25px;
+    padding-right: 0px;
+    padding-bottom: 25px;
+    padding-left: 60px;
+}
+#text_row2 {
+ font-family: Arial,Gotham, Helvetica Neue, Helvetica,sans-serif;
+ font-weight:bold;
+ font-size:25px;	
+ color:#ffffff;
+ position:relative;
+ top:10px;
+ left:10px;
+ width:100%;
+ height: 25px;
+ padding-top: 5px;
+    padding-right: 0px;
+    padding-bottom: 25px;
+    padding-left: 60px;
+}
+#text_row_btn {
+ position:relative;
+ top:10px;
+ left:10px;
+ width:400px;
+ height: 25px;
+ padding-top: 25px;
+    padding-right: 0px;
+    padding-bottom: 25px;
+    padding-left: 55px;
+}
+#appstore_btn {
+ position:relative;
+ left:40px;
+}
+#div-1c {
+ position:relative;
+ top:0px;
+ left:0;
+ width:100%;
+ height: 25px;
+ background-color: #000000;
+}
+#div-headline {
+font-family: Arial,Gotham, Helvetica Neue, Helvetica,sans-serif;
+ font-weight:bold;
+ font-size:45px;	
+ color:#8bc34a;
+ position:relative;
+ top:40px;
+ left:10px;
+ width:100%;
+ height: 25px;
+ padding-top: 5px;
+    padding-right: 0px;
+    padding-bottom: 45px;
+    padding-left: 30px;
+}
+#div-whiteline {
+ position:relative;
+ top:0px;
+ left:0;
+ width:100%;
+ height: 5px;
+ background-color: #ffffff;
+}
+#div-greenline {
+ position:relative;
+ top:25px;
+ left:0;
+ width:100%;
+ height: 5px;
+ background-color: #8bc34a;
+}
+#div-placeholder {
+ position:relative;
+ top:25px;
+ left:0;
+ width:100%;
+}
+#why_reg_row {
+ display:block;
+ position:relative;
+ top:10px;
+ left:10px;
+ width:850px;
+ height: 25px;
+ padding-top: 25px;
+    padding-right: 0px;
+    padding-bottom: 25px;
+    padding-left: 30px;
+}
+#div-placeholder_text {
+ position:relative;
+ top:150px;
+ width:100%;
+ clear:both;
+ overflow:hidden;
+ background-color:#ffffff;
+ padding-bottom: 100px;
+ 
+}
+#why_reg_btn {
+ position:relative;
+ left:60px;
+}
+#why_reg_btn2 {
+ position:relative;
+ left:120px;
+}
+#div-placeholder_how_regina {
+ position:relative;
+ top:175px;
+ left:0;
+ width:100%;
+}
+#div-11 {
+ position:relative;
+    width: 100%;
+}
+
+
 </style>
 <!--The following script tag downloads a font from the Adobe Edge Web Fonts server for use within the web page. We recommend that you do not modify it.-->
 <script>var __adobewebfontsappname__="dreamweaver"</script>
@@ -44,10 +202,9 @@ a:active {
 <!-- Main Container -->
 <div class="container"> 
   <!-- Navigation -->
-  <header> <a href="">
+  <header>
     <h4 class="logo"><img src="pix/home.png" width="50" height="40" alt=""/></h4>
-    </a>
-    <div  style="position:relative; left:0px; top: 20px; width: 1000px;" >
+    <div style="position:relative; left:0px; top: 18px; width: 1000px;" >
      
        <a href="#hero" style="padding:25px;">LÄS TEXT</a>
        <a href="#about" style="padding:25px;">SKICKA TEXT</a>
@@ -58,48 +215,40 @@ a:active {
     </div>
   </header>
   <!-- Hero Section -->
-  <section class="about" style="background-color:#8bc34a" id="about">
-  <div style="position:relative; left:0px; height:500px; top:0px;width:100%">
-  <div style="position:absolute; left:0px; top: 0px;"></div>
-  <div style="position:absolute; right:0px; top:0px;"><img src="pix/regina_android_iphone_apps.png" width="400" height="424" alt=""/></div>
-  
+  <div id="div-1">
+  <div id="div-1b"> 
+    <p id="text_row1">SKICKA TEXT TILL <br> BILÄGARE</p>
+    <p id="text_row2">VIA TELEFON ELLER <br> WEBBEN</p>
+    <p id="text_row_btn"><img src="pix/download_appstore.png" width="150" height="47"> <img id="appstore_btn" src="pix/download_googleplay.png" width="160" height="45"></p>
   </div>
-  </section>
-  <!-- About Section -->
-  <section class="about" id="about">
-    <h2 class="hidden">About</h2>
+  <div id="div-1a"><img style="padding-left:75px;" src="pix/regina_android_iphone_apps.png" width="375" height="399"></div>
+</div>
+
+ <!-- Why Regina section -->
+<div id="div-placeholder">
+ <div id="div-greenline">
+ </div>
+ <div id="div-headline">VARFÖR REGINA!
+ </div>
+ </div>
+ 
+    <div id="div-placeholder">
+    <p id="why_reg_row"><img style="padding-left:20px;" src="pix/why_regina_1.png" width="250" height="154"> <img id="why_reg_btn" src="pix/why_regina_1.png" width="250" height="154"><img id="why_reg_btn2" src="pix/why_regina_1.png" width="250" height="154"></p>
+ </div>
+  <div id="div-placeholder_text">
+   <p class="text_column" style="padding-left:30px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
     <p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
     <p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-    <p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-  </section>
-  <!-- Stats Gallery Section -->
-  <div class="gallery">
-    <div class="thumbnail">
-      <h1 class="stats">1500</h1>
-      <h4>TITLE</h4>
-      <p>One line description</p>
-    </div>
-    <div class="thumbnail">
-      <h1 class="stats">2300</h1>
-      <h4>TITLE</h4>
-      <p>One line description</p>
-    </div>
-    <div class="thumbnail">
-      <h1 class="stats">7500</h1>
-      <h4>TITLE</h4>
-      <p>One line description</p>
-    </div>
-    <div class="thumbnail">
-      <h1 class="stats">9870</h1>
-      <h4>TITLE</h4>
-      <p>One line description</p>
-    </div>
   </div>
+   <!-- End Why regina section -->
+ 
+
   <!-- Parallax Section -->
   <section class="banner">
     <h2 class="parallax">PARALLAX HERO</h2>
     <p class="parallax_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
   </section>
+ 
   <!-- More Info Section -->
   <footer>
     <article class="footer_column">
