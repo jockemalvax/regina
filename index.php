fec2d89397fa7d4c09d9d1ec86b74a2903e42358
Joakim Erlandsson <jockemalvax@gmail.com>
2016-09-21 23:25:13 +0200

    Send message working

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+?>
 <!doctype html>
 <html lang="en-US">
 <head>
@@ -9,16 +13,45 @@
 <script src="js/jquery-3.1.0.js"></script>
 <script src="js/typed.js"></script>
 <script>
+
 $(document).ready(function(){
+
+   $( "#push_message" ).click(function() {
+        var reg_number = $("#reg_number").val();
+		var text_to_push = $("#job").val();
+		/* write function to verify properly*/
+        if (reg_number.length>0 && text_to_push.length>0){
+            
+			$("#text_row2").text(reg_number +" if_och " +text_to_push);
+			
+			$.ajax({
+                type:'POST',
+                url:'getData.php',
+                data:'reg_number='+reg_number+'&text_to_push='+text_to_push,
+                beforeSend:function(html){
+                    $("#loader").show();
+                },
+                success:function(html){
+                       $("#loader").remove();
+                    $('#postlist').append(html); /* Post progress from insert function*/
+                }
+            }); 
+        }
+		else
+		{
+			$("#text_row2").text(reg_number +" else_och " +text_to_push);
+		}
+    });
+
  
  $(function(){
         $(".element").typed({
             strings:
 			[
-			"@ODT401 Din bil är parkerad med lyktorna tända - risk för batteriproblem", 
+			"@ODT401 Din bil är parkerad med lyktorna tända!", 
 			"@HMN338 P-lisor på gatan där din bil är parkerad!", 
 			"@MBO053 Tack, du är en ängel", 
-			"@KGZ342 Vad har du så bråttom till att du riskerar mina barns liv på en 30-väg framför en skola?", 
+			"@KGZ342 Vad har du så bråttom till? Du riskerar liver på våra barn!", 
 			"@OZA032 ”Om du blinkar vänster i en rondell fattar ingen vart du ska!", 
 			],
             typeSpeed: 10,
@@ -368,7 +401,7 @@ font-family: Arial,Gotham, Helvetica Neue, Helvetica,sans-serif;
  <!-- Form -->
  <div id="div-placeholder">
  <div class="form-style-6">
-<form>
+
 <input id="reg_number" type="text" name="field1" placeholder="Fyll i reg. nummer ex: ABC123" />
 
 <select id="job" name="field4" disabled>
@@ -392,8 +425,10 @@ font-family: Arial,Gotham, Helvetica Neue, Helvetica,sans-serif;
 </optgroup>
 </select>   
 
-<input type="submit" value="Skicka" />
-</form>
+<div id="loader" style="display:none;"><img src="pix/loader.gif"></div>
+<input id="push_message" type="submit" value="Skicka" />
+<div id="postlist"></div>
+
 </div>
 </div>
 
